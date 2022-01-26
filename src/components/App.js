b4ffb5f3fb662db9a238dd785a0d129716957600
Ruslan <saharcorez@gmail.com>
2022-01-26 17:59:11 +0300

    feat: popups handlers

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,55 @@ import Main from './Main';
 function App() {
   return (
     <div className="body__container">
-        <Header />
-        <Main />
-        <Footer />
+      <Header />
+      <Main />
+      <Footer />
+      
+      <section class="popup popup-avatar">
+        <div class="popup__container">
+          <button type="button" class="popup__button-clouse" aria-label="Close"></button>
+          <h2 class="popup__title">Обновить аватар</h2>
+          <form class="popup__form popup__avatar-form" name="avatar-form" novalidate>
+            <input class="popup__field popup__field-link-avatar" id="popup__field-link-avatar" name="avatar" type="url"
+              placeholder="Ссылка на картинку" required />
+            <span id="popup__field-link-avatar-error" class="popup__error"></span>
+            <button class="popup__button-save" type="submit">Сохранить</button>
+          </form>
+        </div>
+      </section>
+
+      <section class="popup popup-edit">
+        <div class="popup__container">
+          <button type="button" class="popup__button-clouse" aria-label="Close"></button>
+          <h2 class="popup__title">Редактировать профиль</h2>
+          <form class="popup__form popup__edit-form" name="edit-form" novalidate>
+            <input class="popup__field popup__field-name" id="popup__field-name" name="name" type="text" required
+              minlength="2" maxlength="40" />
+            <span id="popup__field-name-error" class="popup__error"></span>
+            <input class="popup__field popup__field-status" id="popup__field-status" name="about" type="text" required
+              minlength="2" maxlength="200" />
+            <span id="popup__field-status-error" class="popup__error"></span>
+            <button class="popup__button-save" type="submit">Сохранить</button>
+          </form>
+        </div>
+      </section>
+
+    <section class="popup popup-add">
+      <div class="popup__container">
+        <button type="button" class="popup__button-clouse" aria-label="Close"></button>
+        <h2 class="popup__title">Новое место</h2>
+        <form class="popup__form popup__add-form" name="edit-form" novalidate>
+          <input class="popup__field popup__field-mesto" id="popup__field-mesto" name="name" type="text"
+            placeholder="Название" required minlength="2" maxlength="30" />
+          <span id="popup__field-mesto-error" class="popup__error"></span>
+          <input class="popup__field popup__field-link-mesto" id="popup__field-link-mesto" name="link" type="url"
+            placeholder="Ссылка на картинку" required />
+          <span id="popup__field-link-mesto-error" class="popup__error"></span>
+          <button class="popup__button-save" type="submit">Создать</button>
+        </form>
       </div>
+    </section>
+    </div>
   );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,20 +6,32 @@ function Main() {
             <section class="profile">
                 <div class="profile__container">
                     <img src="#" class="profile__avatar" alt="Аватар" />
-                    <button type="button" aria-label="Edit" class="profile__avatar-button"></button>
+                    <button type="button" aria-label="Edit" class="profile__avatar-button" onClick={handleEditAvatarClick}></button>
                     <div class="profile__info">
                         <div class="profile__edit">
                             <h1 class="profile__name">Жак-Ив Кусто</h1>
-                            <button type="button" aria-label="Edit" class="profile__edit-button"></button>
+                            <button type="button" aria-label="Edit" class="profile__edit-button" onClick={handleEditProfileClick}></button>
                             <p class="profile__status">Исследователь океана</p>
                         </div>
                     </div>
-                    <button type="button" aria-label="Add" class="profile__add-button"></button>
+                    <button type="button" aria-label="Add" class="profile__add-button" onClick={handleAddPlaceClick}></button>
                 </div>
             </section>
             <section class="elements"></section>
         </main>
     )
+}
+
+function handleEditAvatarClick() {
+    document.querySelector('.popup-avatar').classList.add('popup_opened');
+}
+
+function handleEditProfileClick() {
+    document.querySelector('.popup-edit').classList.add('popup_opened');
+}
+
+function handleAddPlaceClick() {
+    document.querySelector('.popup-add').classList.add('popup_opened');
 }
 
 export default Main;
