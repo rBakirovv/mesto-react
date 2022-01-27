7a858d06579b7ca7d70c95390e9a530b878c9a66
Ruslan <saharcorez@gmail.com>
2022-01-27 18:50:22 +0300

    fix: Avatar & Edit buttons

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,11 @@ function Main(props) {
             <section className="profile">
                 <div className="profile__container">
                     <img src="#" className="profile__avatar" alt="Аватар" />
-                    <button type="button" aria-label="Edit" className="profile__avatar-button" onClick={props.onEditProfile}></button>
+                    <button type="button" aria-label="Edit" className="profile__avatar-button" onClick={props.onEditAvatar}></button> 
                     <div className="profile__info">
                         <div className="profile__edit">
                             <h1 className="profile__name">Жак-Ив Кусто</h1>
-                            <button type="button" aria-label="Edit" className="profile__edit-button" onClick={props.onEditAvatar}></button>
+                            <button type="button" aria-label="Edit" className="profile__edit-button" onClick={props.onEditProfile}></button>
                             <p className="profile__status">Исследователь океана</p>
                         </div>
                     </div>
