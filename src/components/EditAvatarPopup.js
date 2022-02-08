08a1e47d8fb58bf8079d245e20619e92d25bc43e
Ruslan <saharcorez@gmail.com>
2022-02-09 00:14:13 +0300

    fix: warnings

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import PopupWithForm from './PopupWithForm';
 
 export default function EditAvatarPopup(props) {
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PopupWithForm from './PopupWithForm';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
